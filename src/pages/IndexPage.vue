d8d040bca342dd724cf27d390864f902da670d5a
lucifer3049 <81276617+lucifer3049@users.noreply.github.com>
2024-05-16 16:28:02 +0800

    Update IndexPage.vue
    面試

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,22 +2,16 @@
   <q-page class="row q-pt-xl">
     <div class="full-width q-px-xl">
       <div class="q-mb-xl">
-        <q-input v-model="tempData.name" label="姓名" />
-        <q-input v-model="tempData.age" label="年齡" />
-        <q-btn color="primary" class="q-mt-md">新增</q-btn>
+        <q-input v-model="tempData.name" label="姓名" :rules="[val => val && val.length > 0 || 'Please type name']" />
+        <q-input v-model="tempData.age" label="年齡" :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]" />
+        <q-btn color="primary" class="q-mt-md" @click="addNewPersonnel">新增</q-btn>
       </div>
-
-      <q-table
-        flat
-        bordered
-        ref="tableRef"
-        :rows="blockData"
-        :columns="(tableConfig as QTableProps['columns'])"
-        row-key="id"
-        hide-pagination
-        separator="cell"
-        :rows-per-page-options="[0]"
-      >
+      <!--blockData  -->
+      <q-table flat bordered ref="tableRef" :rows="personnel" :columns="(tableConfig as QTableProps['columns'])"
+        row-key="id" hide-pagination separator="cell" :rows-per-page-options="[0]">
         <template v-slot:header="props">
           <q-tr :props="props">
             <q-th v-for="col in props.cols" :key="col.name" :props="props">
@@ -29,34 +23,14 @@
 
         <template v-slot:body="props">
           <q-tr :props="props">
-            <q-td
-              v-for="col in props.cols"
-              :key="col.name"
-              :props="props"
-              style="min-width: 120px"
-            >
+            <q-td v-for="col in props.cols" :key="col.name" :props="props" style="min-width: 120px">
               <div>{{ col.value }}</div>
             </q-td>
             <q-td class="text-right" auto-width v-if="tableButtons.length > 0">
-              <q-btn
-                @click="handleClickOption(btn, props.row)"
-                v-for="(btn, index) in tableButtons"
-                :key="index"
-                size="sm"
-                color="grey-6"
-                round
-                dense
-                :icon="btn.icon"
-                class="q-ml-md"
-                padding="5px 5px"
-              >
-                <q-tooltip
-                  transition-show="scale"
-                  transition-hide="scale"
-                  anchor="top middle"
-                  self="bottom middle"
-                  :offset="[10, 10]"
-                >
+              <q-btn @click="handleClickOption(btn, props.row)" v-for="(btn, index) in tableButtons" :key="index"
+                size="sm" color="grey-6" round dense :icon="btn.icon" class="q-ml-md" padding="5px 5px">
+                <q-tooltip transition-show="scale" transition-hide="scale" anchor="top middle" self="bottom middle"
+                  :offset="[10, 10]">
                   {{ btn.label }}
                 </q-tooltip>
               </q-btn>
@@ -64,10 +38,7 @@
           </q-tr>
         </template>
         <template v-slot:no-data="{ icon }">
-          <div
-            class="full-width row flex-center items-center text-primary q-gutter-sm"
-            style="font-size: 18px"
-          >
+          <div class="full-width row flex-center items-center text-primary q-gutter-sm" style="font-size: 18px">
             <q-icon size="2em" :name="icon" />
             <span> 無相關資料 </span>
           </div>
@@ -80,7 +51,7 @@
 <script setup lang="ts">
 import axios from 'axios';
 import { QTableProps } from 'quasar';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 interface btnType {
   label: string;
   icon: string;
@@ -118,14 +89,73 @@ const tableButtons = ref([
     status: 'delete',
   },
 ]);
+const personnel = ref([]);
 
 const tempData = ref({
   name: '',
   age: '',
 });
 function handleClickOption(btn, data) {
-  // ...
+  if (btn.status === 'delete') {
+    deletePersonnel(data.id);
+  } else if (btn.status === 'edit') {
+    editPersonnel(data);
+
+  }
 }
+// 查詢人員
+const getPersonnel = async () => {
+  try {
+    const response = await axios.get(`https://dahua.metcfire.com.tw/api/CRUDTest/a`)
+    personnel.value = response.data;
+    console.log(personnel.value);
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+// 新增人員
+const addNewPersonnel = async () => {
+  try {
+    const response = await axios.post(`https://dahua.metcfire.com.tw/api/CRUDTest`, tempData.value);
+    console.log(response.data);
+    getPersonnel();
+  } catch (error) {
+    alert(error)
+  }
+}
+// 編輯人員
+const editPersonnel = async () => {
+  try {
+    await axios.patch(`https://dahua.metcfire.com.tw/api/CRUDTest/${tempData.value}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    // 編輯成功後重新獲取所有人員數據
+    getPersonnel();
+  } catch (error) {
+    console.error("編輯操作失敗:", error);
+    // 考慮添加更友好的錯誤處理機制
+  }
+};
+// 刪除人員
+const deletePersonnel = async (id) => {
+  try {
+    await axios.delete(`https://dahua.metcfire.com.tw/api/CRUDTest/${id}`);
+    // 刪除成功後重新獲取所有人員數據
+    alert("是否確定刪除該筆資料?")
+    getPersonnel();
+  } catch (error) {
+    console.error("刪除操作失敗:", error);
+  }
+};
+
+
+onMounted(() => {
+  getPersonnel();
+});
 </script>
 
 <style lang="scss" scoped>
